4773566d98aea6ae647783e8f067bd7142721d1d
Rumblenex <110209959+Rumblenex@users.noreply.github.com>
2022-09-25 01:19:01 -0400

    Update NexHub.lua

--- NexHub.lua
+++ NexHub.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 8304191830 or game.PlaceId == 8349889591 then
 	pcall(function()
-        loadstring(game:HttpGet(""))() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Rumblenex/Hub-Scripts/main/Anime%20Adventures.lua"))() 
 	end)
 end
