2d15faf883113e7edeefe3a520f47b51ebe01db9
Rumblenex <110209959+Rumblenex@users.noreply.github.com>
2022-10-13 18:49:10 -0400

    Update NexHub-AnimeAdventures.lua

--- NexHub-AnimeAdventures.lua
+++ NexHub-AnimeAdventures.lua
@@ -1,4 +1,4 @@
---v4.1 Nex Hub
+--v4.2 Nex Hub
 --Wait for game to load
 local version = 4.2
 local updateNotes = "\nv4.0\n-Auto farm for Jjk map.\nv4.1\n-Added auto farm for Infinite Caslte.\n -Go to Misc Tab to enable.\nv4.2\n-Added button to rejoin game. Update Log tab."
